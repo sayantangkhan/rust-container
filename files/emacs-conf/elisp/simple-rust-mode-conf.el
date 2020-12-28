8322a2b48d8d1b613760ee1f29030cf5f21dbc76
Sayantan Khan <sayantangkhan@gmail.com>
2020-12-28 12:24:57 +0530

    Fixed exec-path in emacs-conf

--- files/emacs-conf/elisp/simple-rust-mode-conf.el
+++ files/emacs-conf/elisp/simple-rust-mode-conf.el
@@ -4,7 +4,7 @@
 	(add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
 
 (add-hook 'rust-mode-hook 'cargo-minor-mode)
-(add-to-list 'exec-path "$HOME/.cargo/bin")
+(add-to-list 'exec-path "~/.cargo/bin")
 (add-hook 'rust-mode-hook 'flycheck-mode)
 (add-hook 'rust-mode-hook
 	  (lambda () (setq indent-tabs-mode nil)))
